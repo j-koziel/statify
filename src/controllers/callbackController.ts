8437e8daa0364b6e117a2506d112827eea1a797c
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-06-19 18:46:14 +0100

    feat(#5 backend): successfully getting the access token

---
+++ src/controllers/callbackController.ts
@@ -0,0 +1,63 @@
+import { Request, Response, NextFunction } from "express";
+import superagent from "superagent";
+import querystring from "querystring";
+
+import secrets from "../secrets";
+
+const stateKey = "spotify_auth_state";
+const callbackController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
+    // console.log(req.cookies);
+
+    if (state === null) {
+      res.redirect(
+        "/#" +
+          querystring.stringify({
+            error: "state_mismatch",
+          })
+      );
+    } else {
+      res.clearCookie("spotify_auth_state");
+      const authOptions = {
+        url: "https://accounts.spotify.com/api/token",
+        form: {
+          grant_type: "authorization_code",
+          redirect_uri: secrets.redirectUri,
+          code: code,
+        },
+        headers: {
+          Authorization:
+            "Basic " +
+            Buffer.from(`${secrets.clientId}:${secrets.clientSecret}`).toString(
+              "base64"
+            ),
+        },
+        json: true,
+      };
+
+      superagent
+        .post(authOptions.url)
+        .send(authOptions.form)
+        .set("Content-Type", "application/x-www-form-urlencoded")
+        .set("Authorization", authOptions.headers.Authorization)
+        .then((response: superagent.Response) => {
+          const accessToken: string = response.body.access_token;
+          const refreshToken: string = response.body.refresh_token;
+          res.send({ code: accessToken });
+        })
+        .catch((err) => console.log(err));
+    }
+  } catch (err: any) {
+    res.status(400).send(err.message);
+    next(err);
+  }
+};
+
+export default callbackController;
---
+++ src/controllers/loginController.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import querystring from "querystring";
+
+import secrets from "../secrets";
+import generateRandomString from "../utils/generateRandomString";
+
+const loginController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const state: string = generateRandomString(16);
+    res.cookie("spotify_auth_state", state);
+
+    const scope =
+      "user-read-private user-read-email user-top-read user-follow-read user-read-recently-played playlist-read-private playlist-read-collaborative";
+    const params: string = querystring.stringify({
+      response_type: "code",
+      client_id: secrets.clientId,
+      scope,
+      redirect_uri: secrets.redirectUri,
+      state,
+    });
+
+    res.redirect("https://accounts.spotify.com/authorize?" + params);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default loginController;
